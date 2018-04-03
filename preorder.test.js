a923cf4e185aabb3857bcf8559328cb815fca467
science-girl <lindseywoo@gmail.com>
2018-04-03 13:11:36 -0700

    added test cases

--- preorder.test.js
+++ preorder.test.js
@@ -18,6 +18,16 @@ test('traverses a tree with no nodes', () => {
   expect(preorder({})).toEqual([]);
 });
 
+test('traverses a tree with one node', () => {
+  expect(
+    preorder({
+      val: 1,
+      right: null,
+      left: null
+    })
+  ).toEqual([1]);
+});
+
 test('traverses a tree with only left nodes', () => {
   expect(
     preorder({
@@ -27,6 +37,20 @@ test('traverses a tree with only left nodes', () => {
         val: 4,
         right: null,
         left: { val: 2, right: null, left: null }
+      }
+    })
+  ).toEqual([1, 4, 2]);
+});
+
+test('traverses a tree with only right nodes', () => {
+  expect(
+    preorder({
+      val: 1,
+      left: null,
+      right: {
+        val: 4,
+        left: null,
+        right: { val: 2, right: null, left: null }
       }
     })
   ).toEqual([1, 4, 2]);
