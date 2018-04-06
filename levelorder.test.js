c9fb6460b27c8b675adb93217c54744ad2749302
science-girl <lindseywoo@gmail.com>
2018-04-05 17:36:53 -0700

    added non-trivial cases

--- levelorder.test.js
+++ levelorder.test.js
@@ -17,3 +17,35 @@ test('traverses a tree with one node', () => {
     })
   ).toEqual([1]);
 });
+
+test('traverses a tree with multiple left nodes', () => {
+  expect(
+    levelorder({
+      val: 1,
+      right: { val: 3, right: null, left: null },
+      left: {
+        val: 4,
+        right: null,
+        left: { val: 2, right: null, left: null }
+      }
+    })
+  ).toEqual([1, 4, 3, 2]);
+});
+
+test('traverses a tree with multiple left and right nodes', () => {
+  expect(
+    levelorder({
+      val: 1,
+      right: {
+        val: 3,
+        right: { val: 6, right: null, left: null },
+        left: { val: 5, right: null, left: null }
+      },
+      left: {
+        val: 4,
+        right: null,
+        left: { val: 2, right: null, left: null }
+      }
+    })
+  ).toEqual([1, 4, 3, 2, 5, 6]);
+});
--- levelorder_iterative.js
+++ levelorder_iterative.js
@@ -9,8 +9,24 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const levelorderTraversal = function(root) {
+const levelorderTraversal = root => {
   if (!root || Object.keys(root).length === 0) return [];
+
+  const stack = [];
+  let result = [];
+  stack.push(root);
+
+  while (stack.length > 0) {
+    const node = stack.shift();
+    if (typeof node === 'number') result.push(node);
+
+    if (node && node.val) stack.push(node.val);
+    if (node && node.left) stack.push(node.left);
+    if (node && node.right) stack.push(node.right);
+    //console.log(stack);
+  }
+  //console.log(result);
+  return result;
 };
 
 module.exports = levelorderTraversal;
