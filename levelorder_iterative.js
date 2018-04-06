ba51dda259d3c15261d73d102e0c100a4903f87d
science-girl <lindseywoo@gmail.com>
2018-04-05 21:54:21 -0700

    fixed bug resulting from formatting each level into sub-arrays

--- levelorder_iterative.js
+++ levelorder_iterative.js
@@ -17,16 +17,34 @@ const levelorderTraversal = root => {
   stack.push(root);
 
   while (stack.length > 0) {
-    const node = stack.shift();
-    if (typeof node === 'number') result.push(node);
+    let row = [];
+    const temp = [...stack];
 
-    if (node && node.val) stack.push(node.val);
-    if (node && node.left) stack.push(node.left);
-    if (node && node.right) stack.push(node.right);
-    //console.log(stack);
+    temp.forEach(node => {
+      row.push(node.val);
+      if (node && node.left) stack.push(node.left);
+      if (node && node.right) stack.push(node.right);
+
+      stack.shift();
+    });
+
+    result.push(row);
   }
-  //console.log(result);
   return result;
 };
+
+// levelorderTraversal({
+//   val: 1,
+//   right: {
+//     val: 3,
+//     right: { val: 6, right: null, left: null },
+//     left: { val: 5, right: null, left: null }
+//   },
+//   left: {
+//     val: 4,
+//     right: null,
+//     left: { val: 2, right: null, left: null }
+//   }
+// });
 
 module.exports = levelorderTraversal;
