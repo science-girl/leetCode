fd62eadcf249da781bb7a03adb388c5ef8ea7aa2
science-girl <lindseywoo@gmail.com>
2018-04-08 09:02:36 -0700

    added test cases

--- moveZeroes.js
+++ moveZeroes.js
@@ -4,6 +4,8 @@
  */
 const moveZeroes = nums => {
   if (!nums || nums.length === 0) return [];
+
+  for (let i = 0; i < nums.length; i++) {}
 };
 
 module.exports = moveZeroes;
--- moveZeroes.test.js
+++ moveZeroes.test.js
@@ -7,3 +7,19 @@ test('array with no elements', () => {
 test('array with no arguments', () => {
   expect(moveZeroes()).toEqual([]);
 });
+
+test('array with no zeroes in the array', () => {
+  expect(moveZeroes([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+});
+
+test('array with zero at beginning of array', () => {
+  expect(moveZeroes([0, 1, 2, 3, 4])).toEqual([1, 2, 3, 4, 0]);
+});
+
+test('array with all zeroes', () => {
+  expect(moveZeroes([0, 0, 0, 0, 0])).toEqual([0, 0, 0, 0, 0]);
+});
+
+test('array with all > 1 zero', () => {
+  expect(moveZeroes([0, 2, 3, 4, 0])).toEqual([2, 3, 4, 0, 0]);
+});
