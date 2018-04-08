d5a66fcfcb1f3bd3cddacc64cdc8f7a70489cde3
science-girl <lindseywoo@gmail.com>
2018-04-08 14:11:00 -0700

    cannot add same number to get target; added code to add two distinct values

--- twoSum.js
+++ twoSum.js
@@ -5,6 +5,15 @@
  */
 const twoSum = (nums, target) => {
   if (!nums || target < 0 || nums.length === 0) return [];
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i; j < nums.length; j++) {
+      console.log(nums[i], nums[j]);
+      if (i !== j && nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
 };
 
 module.exports = twoSum;
--- twoSum.test.js
+++ twoSum.test.js
@@ -7,3 +7,15 @@ test('array with no elements', () => {
 test('array with no arguments', () => {
   expect(twoSum()).toEqual([]);
 });
+
+test('array values next to each other meeting target value', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test('array values not sequential meeting target value', () => {
+  expect(twoSum([2, 11, 15, 7], 9)).toEqual([0, 3]);
+});
+
+test('array value is two distinct values to get target', () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+});
