e4aabe4bc99fa056acf5a0b30c26d682eec8daf2
science-girl <lindseywoo@gmail.com>
2018-04-08 08:38:00 -0700

    eliminated line that assigns value in array, since we only care abt length anyway

--- removeDuplicates_SortedArray.js
+++ removeDuplicates_SortedArray.js
@@ -8,7 +8,7 @@ const removeDuplicates = nums => {
   let curr = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] < nums[i + 1]) {
-      nums[curr + 1] = nums[i + 1];
+      //nums[curr + 1] = nums[i + 1];
       curr++;
     }
   }
