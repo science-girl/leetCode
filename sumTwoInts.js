d8fe003c9e6fccc9115391f35d038b3d769e82e2
science-girl <lindseywoo@gmail.com>
2018-04-10 11:14:24 -0700

    simplified carry mask assignment

--- sumTwoInts.js
+++ sumTwoInts.js
@@ -5,20 +5,12 @@ const addBits = (a, b) => {
   let carry = 0;
   const bitSum = a.map((num, index) => {
     const sum = num ^ b[index] ^ carry;
-    console.log(sum);
-    if (
-      (num === '1') & (b[index] === '1') ||
-      (num === '1' && carry === '1') ||
-      (b[index] && carry === '1')
-    )
-      carry = 1;
-    console.log('carry', carry);
+    carry = num & b[index] || num & carry || b[index] & carry;
+
     return sum;
   });
   // if there is a remaining carry to push to the front:
-  console.log(bitSum);
   if (carry) bitSum.push(carry);
-  console.log(bitSum);
   return bitSum;
 };
 
@@ -40,8 +32,7 @@ const getSum = (a, b) => {
     .toString(2)
     .split('')
     .reverse();
-  console.log(aBin);
-  console.log(bBin);
+
   // perform bit-wise operations on each element
   const sum =
     aBin.length >= bBin.length
@@ -50,7 +41,5 @@ const getSum = (a, b) => {
 
   return new Number(parseInt(sum.join(''), 2).toString(10));
 };
-
-console.log(getSum(20, 30));
 
 module.exports = getSum;
