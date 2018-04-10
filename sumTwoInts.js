48e50bf59374923394637c86f9136962dff6f4eb
science-girl <lindseywoo@gmail.com>
2018-04-10 09:53:31 -0700

    initial bit addition solution

--- sumTwoInts.js
+++ sumTwoInts.js
@@ -1,4 +1,19 @@
 /**
+ * a is an array that is longer than array b
+ **/
+const addBits = (a, b) => {
+  let carry = 0;
+  const bitSum = a.map((num, index) => {
+    const sum = (num ^ b[index]) | carry;
+    carry = num & b[index];
+    return sum;
+  });
+  // if there is a remaining carry to push to the front:
+  if (carry) bitSum.push(carry);
+  return bitSum;
+};
+
+/**
  * Sum w/o using + or - operators
  * @param {number} a
  * @param {number} b
@@ -6,6 +21,24 @@
  */
 const getSum = (a, b) => {
   if (!a || !b) return 0;
+
+  // convert numbers to binary arrays
+  const aBin = parseInt(a.toString(), 10)
+    .toString(2)
+    .split('')
+    .reverse();
+  const bBin = parseInt(b.toString(), 10)
+    .toString(2)
+    .split('')
+    .reverse();
+
+  // perform bit-wise operations on each element
+  const sum =
+    aBin.length >= bBin.length
+      ? addBits(aBin, bBin).reverse()
+      : addBits(bBin, aBin).reverse();
+
+  return parseInt(sum.join(''), 2).toString(10);
 };
 
 module.exports = getSum;
--- sumTwoInts.test.js
+++ sumTwoInts.test.js
@@ -12,6 +12,14 @@ test('passed with two valid arguments', () => {
   expect(getSum(2, 1)).toEqual(3);
 });
 
+test('passed with two valid arguments requiring binary carry', () => {
+  expect(getSum(2, 3)).toEqual(5);
+});
+
+test('integers of different binary lengths', () => {
+  expect(getSum(100, 1)).toEqual(101);
+});
+
 test('passed with negative numbers', () => {
   expect(getSum(-2, -1)).toEqual(-3);
 });
